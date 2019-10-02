bb289897910b6b4af65ca16284d0308f18c3074e
Muhammad Fari <fari.msenju@gmail.com>
2019-10-02 11:46:00 +0700

    Create GraphQL Schema with JavaScript
    Change query Graphql from buildSchema to JavaScript Schema GraphQL.

--- index.js
+++ index.js
@@ -1,58 +1,97 @@
-const { graphql, buildSchema } = require('graphql');
+const { 
+    GraphQLID,
+    GraphQLBoolean,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLList
+ } = require('graphql');
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 
 const PORT = process.env.PORT || 5678;
 const server = express();
 
-const schema = buildSchema(`
-type Video {
-    id: ID,    
-    title: String,
-    duration: Int,
-    watched: Boolean
-}
+const VIDEOS = [
+    {
+        id: '1',
+        title: 'Learning GraphQL',
+        duration: 180,
+        watched: true
+    },
 
-type Query {
-    videos: [Video]
-}
+    {
+        id: '2',
+        title: 'Learning JS',
+        duration: 100,
+        watched: true
+    },
 
-type Schema {
-    query: Query
-}
-
-`);
-
-const resolvers = {
-    videos: () => {
-        return [
-            {
-                id: '1',
-                title: 'Learning GraphQL',
-                duration: 180,
-                watched: true
-            },
-
-            {
-                id: '2',
-                title: 'Learning JS',
-                duration: 100,
-                watched: true
-            },
-
-            {
-                id: '3',
-                title: 'Learning PHP',
-                duration: 120,
-                watched: false
-            }
-        ]
+    {
+        id: '3',
+        title: 'Learning PHP',
+        duration: 120,
+        watched: false
     }
-};
+];
+
+const videoType = new GraphQLObjectType({
+    name: 'Video',
+    description: 'A video for KODE platform',
+    fields: {
+        id: {
+            type: GraphQLID,
+            description: 'The id of the video'
+        },
+        title: {
+            type: GraphQLString,
+            description: 'The title of the video'
+        },
+        duration: {
+            type: GraphQLInt,
+            description: 'Duration of the video'
+        },
+        watched: {
+            type: GraphQLBoolean,
+            description: 'Whether or not the video has been watched.'
+        }
+    }
+});
+
+const queryType = new GraphQLObjectType({
+    name: 'QueryType',
+    description: 'The root of query type',
+    fields: {
+        video: {
+            type: videoType,
+            resolve: () => new Promise((resolve) => {
+                resolve({
+                    id: '1',
+                    title: `What's new in GraphQL`,
+                    duration: 180,
+                    watched: false
+                })
+            })
+        },
+        videos: {
+            type: new GraphQLList(videoType),
+            resolve: () => {
+                return new Promise((resolve) => {
+                    resolve(VIDEOS);
+                })
+            }
+        }
+    }
+});
+
+const schema = new GraphQLSchema({
+    query: queryType
+});
+
 
 server.use('/graphql', graphqlHTTP({
     schema: schema,
-    rootValue: resolvers,
     graphiql: true
 }));
 
